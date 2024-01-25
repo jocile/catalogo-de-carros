a10b5b2bc467335ed3cb169b28fc8d68ca83f9be
jocile <jocile@gmail.com>
2024-01-25 19:20:23 -0300

    ✨ Editando o menu Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,10 +4,22 @@ import './styles.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar main-nav">
-      <Link to="/" className="nav-logo-text">
-        <h4 className="text-primary">ViaCEP</h4>
-      </Link>
+    <nav className="navbar navbar-expand-md navbar-light bg-primary main-nav">
+      <div className="container-fluid">
+        <Link to="/" className="nav-logo-text">
+          <h4>Carros Top</h4>
+        </Link>
+        <div className="collapse navbar-collapse" id="carrostop-navbar">
+          <ul className="navbar-nav offset-md-2 main-menu">
+            <li>
+              <a href="link" className="active">Home</a>
+            </li>
+            <li>
+              <a href="link">Catálogo</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
