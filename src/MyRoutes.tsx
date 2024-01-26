6cabcc3fb9deb5a2391ea1f793e3a353bf4315da
jocile <jocile@gmail.com>
2024-01-26 16:59:37 -0300

    ⬆ Upgrade: Atualizacao para NodeJS 18

---
+++ src/MyRoutes.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from 'pages/Home';
+import Navbar from 'components/Navbar';
+
+const MyRoutes = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/"element={<Home />} />      
+    </Routes>
+  </BrowserRouter>
+);
+
+export default MyRoutes;
