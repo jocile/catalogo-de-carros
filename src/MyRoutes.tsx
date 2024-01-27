00f1bace03411e932dbfc0c4aace533c73a717c0
jocile <jocile@gmail.com>
2024-01-26 22:18:26 -0300

    ✨ Componente ProductCard adicionado

--- src/MyRoutes.tsx
+++ src/MyRoutes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
+import Catalog from 'pages/Catalog';
 
 const MyRoutes = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="/"element={<Home />} />      
+      <Route path="/"element={<Home />} />
+      <Route path="/products" element={<Catalog />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ src/pages/Catalog/index.tsx
@@ -0,0 +1,49 @@
+import './styles.css';
+
+import ProductCard from 'components/ProductCard';
+
+const Catalog = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Catalog;
