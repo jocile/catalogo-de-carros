f8857a8362044c49a43f52b59a43383a3623f918
jocile <jocile@gmail.com>
2024-01-26 22:32:29 -0300

    ✨ Componente busca adicionado

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,10 +1,14 @@
 import './styles.css';
 
 import ProductCard from 'components/ProductCard';
+import SearchCard from 'components/SearchCard';
 
 const Catalog = () => {
   return (
     <>
+    <div className="container">
+        <SearchCard />
+      </div>
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4">
