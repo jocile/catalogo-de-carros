997c821f9af43187bafced1fc2726cc39eb63abf
jocile <jocile@gmail.com>
2024-01-26 21:49:59 -0300

    🚧Home: add imagens

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,26 @@
 import { Link } from 'react-router-dom';
-
 import './styles.css';
+import CarImage from 'assets/images/car-header.png';
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <Link to="/cepsearch">
-                <button className="btn btn-primary btn-lg start-button">Iniciar</button>
-            </Link>
-        </div>
-    );
-}
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-card-content">
+            <h5>O carro perfeito para você</h5>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
+          </div>
+          <div className="home-card-image">
+            <img src={CarImage} alt="Imagem do carro" />
+          </div>
+        </div>        
+      </div>
+    </>
+  );
+};
 
 export default Home;
